e718633b9e5479606df681fcfa5aaac6d83d235f
jorgedan88 <jorge.olivera@istea.com.ar>
2023-10-03 23:14:15 -0300

    Se crea el proyecto del tipo MVC

---
+++ Models/ErrorViewModel.cs
@@ -0,0 +1,8 @@
+namespace herramientas_parcial1_OliveraJorgeDaniel.Models;
+
+public class ErrorViewModel
+{
+    public string? RequestId { get; set; }
+
+    public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+}
